92dcbe5e85d30fc9247cc97533c0612861153e68
BitpixAgency <77122344+BitpixAgency@users.noreply.github.com>
2022-02-03 21:23:50 +0000

    0.92 - Add logout button to verify email view

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -26,6 +26,10 @@
                         <span class="fw-bold text-gray-700">Did’t receive an email?</span>
                         <button type="submit" class="btn btn-link fw-bolder">Resend</button>
                     </form>
+                    <form action="/logout" method="POST">
+                            @csrf
+                            <button type="submit" class="btn btn-link bg-transparent menu-link px-5">Sign Out</button>
+                        </form>
                 </div>
             </div>
             <div class="d-flex flex-row-auto bgi-no-repeat bgi-position-x-center bgi-size-contain bgi-position-y-bottom min-h-100px min-h-lg-350px" style="background-image: url(assets/media/illustrations/sigma-1/17.png"></div>
