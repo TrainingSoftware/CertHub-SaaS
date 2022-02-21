72f888f91736e13b54fb2c84b6ba1ad30abd5866
BitpixAgency <77122344+BitpixAgency@users.noreply.github.com>
2022-02-21 15:28:06 +0000

    1.54 - Fix header

--- resources/views/livewire/list-qualification-types.blade.php
+++ resources/views/livewire/list-qualification-types.blade.php
@@ -3,18 +3,32 @@
         <div id="kt_content_container" class="container-xxl">
             <div class="card">
                 @if(Auth::user()->companies->first()->qualificationtypes->count() != 0 )
-                <div class="card-header border-0 pt-6">
-                    <div class="card-title">
-                        <div class="d-flex align-items-center position-relative my-1">
-                            <span class="svg-icon svg-icon-1 position-absolute ms-6">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                    <rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546" height="2" rx="1" transform="rotate(45 17.0365 15.1223)" fill="black" />
-                                    <path d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z" fill="black" />
-                                </svg>
-                            </span>
-                            <input type="text" wire:model="searchTerm" class="form-control form-control-solid w-250px ps-14" placeholder="Search qualification types" autocomplete="off"/>
-                        </div>
-                    </div>
+                <div class="card-header border-0 py-6">
+                    <div class="card-title w-100">
+                        <div class="row w-100">
+                            <div class="col-3">
+								<div class="d-flex align-items-center position-relative my-1">
+									<span class="svg-icon svg-icon-1 position-absolute ms-6">
+										<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+											<rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546" height="2" rx="1" transform="rotate(45 17.0365 15.1223)" fill="black" />
+											<path d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z" fill="black" />
+										</svg>
+									</span>
+									<input type="text" class="form-control form-control-solid ps-14" placeholder="Search qualification types" wire:model="searchTerm"/>
+								</div>
+							</div>
+							<div class="col-2 mt-3">
+                                <a href="/qualificationtypes">
+                                    <span class="svg-icon svg-icon-muted svg-icon-1hx">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+                                            <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+                                        </svg>
+                                    </span>
+                                </a>
+                            </div>
+						</div>
+					</div>
                 </div>
                 @endif
                 @if(Auth::user()->companies->first()->qualificationtypes->count() == 0 )
