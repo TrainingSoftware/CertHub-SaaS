5555aef904543bc7d1fc168ccb68de3d163b13f4
BitpixAgency <77122344+BitpixAgency@users.noreply.github.com>
2022-03-08 11:12:48 +0000

    1.96 - Qualification Course Meta UI

--- resources/views/qualifications/show.blade.php
+++ resources/views/qualifications/show.blade.php
@@ -5,7 +5,8 @@
     <div class="toolbar d-flex flex-stack mb-3 mb-lg-5" id="kt_toolbar">
         <div id="kt_toolbar_container" class="container d-flex flex-stack flex-wrap">
             <div class="page-title d-flex flex-column me-5 py-2">
-                <h1 class="d-flex flex-column text-dark fw-bolder fs-3 mb-0">Qualification: {{ $qualification->qualificationtype->name }} </h1>
+                <h1 class="d-flex flex-column text-dark fw-bolder fs-3 mb-0">Qualification: {{
+                    $qualification->qualificationtype->name }} </h1>
                 <ul class="breadcrumb breadcrumb-separatorless fw-bold fs-7 pt-1">
                     <li class="breadcrumb-item text-muted">
                         <a href="/home" class="text-muted text-hover-primary">Home</a>
@@ -30,17 +31,19 @@
                 <div class="card-header cursor-pointer">
                     <div class="card-title m-0">
                         <h3 class="fw-bolder m-0">
-                            {{ optional($qualification->employee)->firstname }}'s {{ optional($qualification->qualificationtype)->name }} Details
+                            {{ optional($qualification->employee)->firstname }}'s {{
+                            optional($qualification->qualificationtype)->name }} Details
                         </h3>
                         @if($qualification->expiry_date === null)
                         <span class="badge badge-success ms-5">Valid</span>
-                        @elseif($qualification->expiry_date->format('m') == Carbon\Carbon::today()->format('m') && $qualification->expiry_date > Carbon\Carbon::today())
+                        @elseif($qualification->expiry_date->format('m') == Carbon\Carbon::today()->format('m') &&
+                        $qualification->expiry_date > Carbon\Carbon::today())
                         <span class="badge badge-warning ms-5">Expiring</span>
-                        @elseif($qualification->expiry_date < Carbon\Carbon::today())
-                         <span class="badge badge-danger ms-5">Expired</span>
-                        @else
-                        <span class="badge badge-success ms-5">Valid</span>
-                        @endif
+                        @elseif($qualification->expiry_date < Carbon\Carbon::today()) <span
+                            class="badge badge-danger ms-5">Expired</span>
+                            @else
+                            <span class="badge badge-success ms-5">Valid</span>
+                            @endif
                     </div>
                     <div class="flex-end align-self-center">
                         @if($qualification->getMedia('qualification')->first())
@@ -50,12 +53,15 @@
                         </form>
                         @endif
                         @if($qualification->expiry_date == null)
-                        <a href="/qualifications/{{ $qualification->id }}/edit" class="btn btn-sm btn-primary">Edit Qualification</a>
-                        @elseif($qualification->expiry_date < Carbon\Carbon::today())
-                        <a href="/renewals/create?qualification={{ $qualification->id }}" class="btn btn-sm btn-warning">Add Renewal</a>
-                        @else
-                        <a href="/qualifications/{{ $qualification->id }}/edit" class="btn btn-sm btn-primary">Edit Qualification</a>
-                        @endif
+                        <a href="/qualifications/{{ $qualification->id }}/edit" class="btn btn-sm btn-primary">Edit
+                            Qualification</a>
+                        @elseif($qualification->expiry_date < Carbon\Carbon::today()) <a
+                            href="/renewals/create?qualification={{ $qualification->id }}"
+                            class="btn btn-sm btn-warning">Add Renewal</a>
+                            @else
+                            <a href="/qualifications/{{ $qualification->id }}/edit" class="btn btn-sm btn-primary">Edit
+                                Qualification</a>
+                            @endif
                     </div>
                 </div>
                 <div class="card-body p-9">
@@ -63,14 +69,17 @@
                         <label class="col-lg-4 fw-bold text-muted">Employee</label>
                         <div class="col-lg-8">
                             <span class="fw-bolder fs-6 text-gray-800">
-                            <a href="/employees/{{ optional($qualification->employee)->id }}">{{ optional($qualification->employee)->firstname }} {{ optional($qualification->employee)->lastname }}</a>
+                                <a href="/employees/{{ optional($qualification->employee)->id }}">{{
+                                    optional($qualification->employee)->firstname }} {{
+                                    optional($qualification->employee)->lastname }}</a>
                             </span>
                         </div>
                     </div>
                     <div class="row mb-7">
                         <label class="col-lg-4 fw-bold text-muted">Qualification Type</label>
                         <div class="col-lg-8">
-                            <span class="fw-bolder fs-6 text-gray-800">{{ $qualification->qualificationtype->name }}</span>
+                            <span class="fw-bolder fs-6 text-gray-800">{{ $qualification->qualificationtype->name
+                                }}</span>
                         </div>
                     </div>
                     <div class="row mb-7">
@@ -82,7 +91,8 @@
                     <div class="row mb-7">
                         <label class="col-lg-4 fw-bold text-muted">Expiry Date</label>
                         <div class="col-lg-8">
-                            <span class="fw-bolder fs-6 text-gray-800">{{ optional($qualification->expiry_date)->format('d/m/Y') }}</span>
+                            <span class="fw-bolder fs-6 text-gray-800">{{
+                                optional($qualification->expiry_date)->format('d/m/Y') }}</span>
                         </div>
                     </div>
                     <div class="row mb-7">
@@ -116,6 +126,65 @@
                         </div>
                     </div>
                 </div>
+            </div>
+            <div class="card mb-5 mb-xl-8">
+                <!--begin::Header-->
+                <div class="card-header border-0 pt-5">
+                    <h3 class="card-title align-items-start flex-column">
+                        <span class="card-label fw-bolder fs-3 mb-1">Renewal offers</span>
+                        <span class="text-muted mt-1 fw-bold fs-7">X available</span>
+                    </h3>
+                </div>
+                <!--end::Header-->
+                <!--begin::Body-->
+                <div class="card-body py-3">
+                    <!--begin::Table container-->
+                    <div class="table-responsive">
+                        <!--begin::Table-->
+                        <table class="table table-row-bordered table-row-gray-100 align-middle gs-0 gy-3">
+                            <!--begin::Table head-->
+                            <thead>
+                                <tr class="fw-bolder text-muted">
+                                    <th class="">Date</th>
+                                    <th class="">Location</th>
+                                    <th class="">Duration</th>
+                                    <th class="">Price</th>
+                                </tr>
+                            </thead>
+                            <!--end::Table head-->
+                            <!--begin::Table body-->
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <a href="#" class="text-dark fw-bolder text-hover-primary d-block mb-1 fs-6">
+                                            05/28/2020</a>
+                                    </td>
+                                    <td>
+                                        <a href="#"
+                                            class="text-dark fw-bolder text-hover-primary d-block mb-1 fs-6">Brasil</a>
+                                    </td>
+                                    <td>
+                                        <a href="#" class="text-dark fw-bolder text-hover-primary d-block mb-1 fs-6">2
+                                            Days</a>
+                                    </td>
+                                    <td>
+                                        <a href="#"
+                                            class="text-dark fw-bolder text-hover-primary d-block mb-1 fs-6">£299.00
+                                        </a>
+                                    </td>
+                                    <td class="text-end">
+                                        <a href="/order/" class="btn btn-primary btn-sm">Book now</a>
+                                    </td>
+                                </tr>
+
+                            </tbody>
+                            <!--end::Table body-->
+                        </table>
+                        <!--end::Table-->
+                    </div>
+                    <!--end::Table container-->
+                </div>
+                <!--begin::Body-->
             </div>
         </div>
     </div>
